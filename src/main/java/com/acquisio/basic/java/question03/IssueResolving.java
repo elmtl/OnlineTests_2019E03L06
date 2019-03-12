e72b13e9bc3c8d76b757f3d5503a9a1848ab5463
elmtl <48460003+elmtl@users.noreply.github.com>
2019-03-11 22:08:53 -0400

    Update IssueResolving.java

--- src/main/java/com/acquisio/basic/java/question03/IssueResolving.java
+++ src/main/java/com/acquisio/basic/java/question03/IssueResolving.java
@@ -8,16 +8,24 @@ package com.acquisio.basic.java.question03;
  * IMPORTANT: Add all missing javadoc and/or unit tests that you think is necessary.
  */
 public class IssueResolving {
-    public static void main(String[] args) {
-        IssueResolving instance = new IssueResolving();
-        System.out.println(instance.factorial(7));
-    }
+	public static void main(String[] args) {
+		IssueResolving instance = new IssueResolving();
+		System.out.println(instance.factorial(7));
+	}
 
-    int factorial(int n) {
-        // TODO: Fix code here.
-        int result = 0;
-        result = factorial(n - 1) * n;
-        return result;
-    }
+	/***
+	 * To calulate the factorial ex: 3!  = 3 x 2 x 1
+	 * @param n input value
+	 * @return calculated factorial
+	 */
+	int factorial(int n) {
+		// TODO: Fix code here.
+		if (n > 0) {
+			int result = 0;
+			result = factorial(n - 1) * n;
+			return result;
+		}
+		return 1;
+	}
 
 }
